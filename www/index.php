e169497a47905b0c8829068305bdd888553d3c8d
Yves skrzypczyk <y.skrzypczyk@gmail.com>
2022-03-22 20:38:47 +0100

    start PA - step 1

--- www/index.php
+++ www/index.php
@@ -1,1 +1,13 @@
-Bonjour
+<?php
+
+//récupérer ce que l'on a dans l'url
+//exemple http://localhost/ajouter-utilisateur
+//je dois récupérer "ajout-utilisateur"
+
+
+
+//A quoi correspond ajout-utilisateur
+//un tableau association qui aura la clé ajout-utilisateur
+// et contiendra le nom d'une classe et la methode à appeler
+// exemple : ajout-utilisateur ->   class User() et method add()
+
