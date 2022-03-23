c0ef5190a49bbbe940d316316e69c115d9afcadd
Yves skrzypczyk <y.skrzypczyk@gmail.com>
2022-03-23 12:34:07 +0100

    routing via class

--- www/index.php
+++ www/index.php
@@ -16,56 +16,12 @@ $uri = strtolower($requestExploded[0]); // /ajout-utilisateur
 // et contiendra le nom d'une classe et la methode à appeler
 // exemple : ajout-utilisateur ->   class User() et method add()
 
-require "Core\Routing.class.php";
+require "Core/Routing.class.php";
 $routing = new Core\Routing();
-$routing->setAction();
+$routing->setAction($uri);
 $routing->run();
 
-/*
-if(!file_exists("routes.yml")){
-	die("Fichier routes.yml introuvable");
-}
-$routes = yaml_parse_file("routes.yml");
 
-
-if( empty($routes[$uri]) 
-	|| empty($routes[$uri]["controller"])  
-	|| empty($routes[$uri]["action"])){
-	die("Page 404");
-}
-
-$controller = $routes[$uri]["controller"];//Main
-$action = $routes[$uri]["action"]; //index
-
-//Faites une instance du controller et appelez la bonne action
-//au final on doit avoir le echo qui se lance
-
-
-if(!file_exists("Controller/".$controller.".class.php")){
-	die("Le fichier Controller/".$controller.".class.php nexiste pas");
-}
-include "Controller/".$controller.".class.php";
-
-
-//Instance dynamique
-// new User mais un new \App\Controller\User
-//Obligation d'écrire le chemin complet dans une instance dynamique
-$controller = "\\App\\Controller\\".$controller;
-
-if(!class_exists($controller)){
-	die("Le controller ".$controller." n'existe pas");
-}
-$object = new $controller();
-
-
-if(!method_exists($object, $action)){
-	die("L'action ".$action." n'existe pas");
-}
-$object->$action();
-
-*/
-
-
-
+echo $routing->getUri("Admin", "dashboard");
 
 
