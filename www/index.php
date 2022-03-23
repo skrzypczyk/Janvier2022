aa8938011a99c024abb7e5d90797ce48bdf130a3
Yves skrzypczyk <y.skrzypczyk@gmail.com>
2022-03-23 09:54:29 +0100

    init routing

--- www/index.php
+++ www/index.php
@@ -2,12 +2,20 @@
 
 //récupérer ce que l'on a dans l'url
 //exemple http://localhost/ajouter-utilisateur
-//je dois récupérer "ajout-utilisateur"
+//je dois récupérer "/ajout-utilisateur"
+//attention j'ai http://localhost/modifier-utilisateur?id=4
+//on doit récuperer : /modifier-utilisateur
 
-
+????
+$uri = ????? // /ajout-utilisateur
 
 //A quoi correspond ajout-utilisateur
 //un tableau association qui aura la clé ajout-utilisateur
 // et contiendra le nom d'une classe et la methode à appeler
 // exemple : ajout-utilisateur ->   class User() et method add()
+
+
+???
+$controller = ???;//Main
+$action = ???;//index
 
